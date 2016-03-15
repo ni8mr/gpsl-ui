ae4c8a97ae49550252becbcf0b26d91bb4cadaa5
ni8mr2 <nilpaddo@gmail.com>
2016-03-15 13:57:09 +0600

    Partitioned js to external file

---
+++ selection.js
@@ -0,0 +1,61 @@
+<script language="JavaScript">
+    (function() {
+               var url = "./dummy.json";
+               //var $select = $('#myselect')
+               $.getJSON(url)
+               .done(function( data ) {
+                    // $.each(data.items, function(i, item){
+                    //   $select.append($('<option>').data[name]);
+                    // });
+                    $('#select-district').empty();
+                    $.each(data, function(key, value) {
+                      if(key == "Districts"){
+
+                        $('#select-district').append('<option value=' + data["Districts"].id + '>' + data["Districts"].name + '</option>');
+
+                        var selected = document.getElementById("select-district"); // Use jquery selector
+
+                        var selectedDistrict = selected.options[selected.selectedIndex].value;
+
+                        $.each(data, function(key, value) {
+                            if(key == "Districts"){
+                              if(data["Districts"].id == selectedDistrict){
+                                subDistricts = data["Districts"].Subdistricts;
+                                for(i =0; i<subDistricts.length; i++){
+                                  $.each(data, function(key, value) {
+                                    if(key == "Subdistricts"){
+                                      if(data["Subdistricts"].id == subDistricts[i]){
+                                        $('#select-thana').append('<option value=' + data["Subdistricts"].id + '>' + data["Subdistricts"].name + '</option>');
+
+                                        var selected = document.getElementById("select-thana")
+
+                                        var selectedThana = selected.options[selected.selectedIndex].value;
+
+                                        $.each(data, function(key, value) {
+                                          if(key == "Districts"){
+                                            if(data["Districts"].id == selectedDistrict){
+                                              subDistricts = data["Districts"].Subdistricts;
+                                              for(i =0; i<subDistricts.length; i++){
+                                                $.each(data, function(key, value) {
+                                                  if(key == "Subdistricts"){
+                                                    for(i =0; i< data["Subdistricts"].Postcodes.length; i++ ){
+                                                      $('#select-post-code').append('<option value=' + i + '>' + data["Subdistricts"].Postcodes[i] + '</option>');
+                                                    }
+                                                  }
+                                                });
+                                              }
+                                            }
+                                          }
+                                        });
+                                      }
+                                    }
+                                  });
+                                }
+                              }
+                            }
+                        });
+                      }
+                    });
+                });
+              })();
+</script>
