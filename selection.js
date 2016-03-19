1567a3895e75624b65c681e5d2a33747763cc6f9
ni8mr2 <nilpaddo@gmail.com>
2016-03-19 12:17:13 +0600

    Make some change

--- selection.js
+++ selection.js
@@ -13,7 +13,7 @@
       subdistricts.push(data['Subdistricts']);
        $('#country').empty();
         for(i=0; i<countries[0].length; i++){
-            $('#country').append('<option data-tokens="' + countries[0][i]["name"] +'" value="' + i + '">' + countries[0][i]["name"] + '</option>');
+            $('#country').append('<option data-tokens="' + countries[0][i].name +'" value="' + i + '">' + countries[0][i].name + '</option>');
         };
         $('#country').change(function(){
                 var selected = $( this ).val();
