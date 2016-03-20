153c60189bd2a3d5cdb3fc0128e8ad28b2f3fc44
TanvirM <tanvir.mohammad@gmail.com>
2016-03-20 12:56:24 +0600

    fix

--- selection.js
+++ selection.js
@@ -1,96 +1,33 @@
-
 (function() {
-  var url = "dummy.json";
-    countries = [];
-    divisions = [];
-    districts = [];
-    subdistricts = [];
+  var url = "dummy.json",
+    countries = [],
+    divisions = [],
+    districts = [],
+    subdistricts = [],
+    input_country =$('#country');
   $.getJSON(url)
     .done(function( data ) {
-      countries.push(data['Country']);
-      divisions.push(data['Divisions']);
-      districts.push(data['Districts']);
-      subdistricts.push(data['Subdistricts']);
-       $('#country').empty();
+        countries.push(data['Country']);
+        divisions.push(data['Divisions']);
+        districts.push(data['Districts']);
+        subdistricts.push(data['Subdistricts']);
+
+        // input_country.empty();
+
+        var option_string = '';
+
         for(var i=0; i<countries[0].length; i++){
-            $('#country').append('<option data-tokens="' + countries[0][i]["name"].toLowerCase() +'" value="' + countries[0][i]["id"] + '">' + countries[0][i]["name"] + '</option>');
-        };
-        $('#country').change(function(){
-                var selected = $( this ).val();
-                console.log(selected);
-            });
+
+            option_string += '<option data-tokens="' + countries[0][i]["name"].toLowerCase() +'" value="' + countries[0][i]["id"] + '">' + countries[0][i]["name"] + '</option>';
+// ;            ('<option data-tokens="' + countries[0][i]["name"].toLowerCase() +'" value="' + countries[0][i]["id"] + '">' + countries[0][i]["name"] + '</option>');
+        }
+
+        console.log(option_string);
+        // data-live-search="true"
+        input_country.empty().append(option_string).change(function(){
+            var selected = $(this).val();
+            console.log(selected);
+        }).selectpicker('refresh');
+
       });
 })();
-
-              //  $.getJSON(url)
-              //  .done(function( data ) {
-              //      var JSON = [];
-              //       for (var i in data){
-              //         JSON.push(data[i]);
-              //       }
-              //      //console.log(JSON);
-              //      // for (i=0; i<JSON.length; i++){
-              //      //     $('#select-district').empty();
-              //      //
-              //      // }
-              //
-              //       $('#select-district').empty();
-              //       $.each(data, function(key, value) {
-              //         if(key == "Districts"){
-              //
-              //           $('#select-district').append('<option value=' + data["Districts"].id + '>' + data["Districts"].name + '</option>');
-              //
-              //             // $("#select-district").change(function() {
-              //             //     var selected = $( this ).val();
-              //             //     $.each(data, function(key, value) {
-              //             //       if(key == "Districts"){
-              //             //         if(data["Districts"].id == selected){
-              //             //           subDistricts = data["Districts"].Subdistricts;
-              //             //           for(i =0; i<subDistricts.length; i++){
-              //             //             $.each(data, function(key, value) {
-              //             //                 if(key == "Subdistricts"){
-              //             //                 if(data["Subdistricts"].id == subDistricts[i]){
-              //             //                     $('#select-thana').append('<option value=' + data["Subdistricts"].id + '>' + data["Subdistricts"].name + '</option>');
-              //             //                     $("#select-thana").change(function() {
-              //             //                       var selectedThana = $( this ).val();
-              //             //                         $.each(data, function(key, value) {
-              //             //                           if(key == "Districts"){
-              //             //                             if(data["Districts"].id == selected){
-              //             //                               subDistricts = data["Districts"].Subdistricts;
-              //             //                               for(i =0; i<subDistricts.length; i++){
-              //             //                                 $.each(data, function(key, value) {
-              //             //                                   if(key == "Subdistricts"){
-              //             //                                     for(i =0; i< data["Subdistricts"].Postcodes.length; i++ ){
-              //             //                                         $('#select-post-code').append('<option value=' + i + '>' + data["Subdistricts"].Postcodes[i] + '</option>');
-              //             //                                     }
-              //             //                          }
-              //             //                       });
-              //             //                     }
-              //             //                   }
-              //             //                 }
-              //             //               });
-              //             //                     });
-              //             //
-              //             //               // var selected = document.getElementById("select-thana")
-              //             //               //
-              //             //               // var selectedThana = selected.options[selected.selectedIndex].value;
-              //             //
-              //             //
-              //             //             }
-              //             //           }
-              //             //         });
-              //             //       }
-              //             //     }
-              //               }
-              //           //});
-              //             //});
-              //
-              //           // var selected = document.getElementById("select-district"); // Use jquery selector
-              //           //
-              //           // var selectedDistrict = selected.options[selected.selectedIndex].value;
-              //
-              //
-              //         //}
-              //       });
-              //   });
-              // })();
