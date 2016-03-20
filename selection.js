e1d5c0bbdd218c2439a08e1f67d28a3e58ae46f7
TanvirM <tanvir.mohammad@gmail.com>
2016-03-20 12:57:28 +0600

    JS cleaned

--- selection.js
+++ selection.js
@@ -12,18 +12,12 @@
         districts.push(data['Districts']);
         subdistricts.push(data['Subdistricts']);
 
-        // input_country.empty();
-
         var option_string = '';
 
         for(var i=0; i<countries[0].length; i++){
-
             option_string += '<option data-tokens="' + countries[0][i]["name"].toLowerCase() +'" value="' + countries[0][i]["id"] + '">' + countries[0][i]["name"] + '</option>';
-// ;            ('<option data-tokens="' + countries[0][i]["name"].toLowerCase() +'" value="' + countries[0][i]["id"] + '">' + countries[0][i]["name"] + '</option>');
         }
 
-        console.log(option_string);
-        // data-live-search="true"
         input_country.empty().append(option_string).change(function(){
             var selected = $(this).val();
             console.log(selected);
