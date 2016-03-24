9b53b9c9579d712154bad5dc4b533a9bf0f04006
ni8mr2 <nilpaddo@gmail.com>
2016-03-24 08:34:10 +0600

    Testing if the we can catch the submitted values.

--- submit.js
+++ submit.js
@@ -1,5 +1,5 @@
 $(function() {
-    $("#depot-submit-form").on('submit', function(e) {
+    $("#depot-submit-form").submit(function(e) {
         e.preventDefault();
         var data = $("#depot-submit-form").serializeArray();
         console.log(data);
