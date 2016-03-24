9b6c6d12bdecbd81c5fb0fc1c3ea544808779c5a
ni8mr2 <nilpaddo@gmail.com>
2016-03-24 11:21:13 +0600

    Check if we can grab the empty values of input fields.

--- submit.js
+++ submit.js
@@ -4,7 +4,9 @@ $(function() {
         var data = $("#depot-submit-form").serializeArray();
         console.log(data);
         for(var i=0; i<data.length; i++){
-            console.log(data[i]);
+            if(data[i].value.length == 0){
+                console.log(data[i].value);
+            }
         }
     });
 });
