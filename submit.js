fed557c603dde70515834c32711717a8f4d73e45
ni8mr2 <nilpaddo@gmail.com>
2016-03-24 18:12:45 +0600

    Add codes for validation. (v8- Made the empty boxes red. Made some changes for catching current div)

--- submit.js
+++ submit.js
@@ -3,15 +3,17 @@
     e.preventDefault();
     $(this).find('input').each(function(){
       if($(this).val().length == 0){
-         $("<span>Field should not be empty!</span>").insertAfter( $(this));
-          $(this).addClass("has-error");
+         $("<span>Field should not be empty!</span>").insertAfter($(this));
+          $(this).closest(".form-group.row").addClass("has-error");
+          //$(this).addClass("has-error");
           //$('input').closest("div.form-group.row").addClass("has-error");
       }
     });
       $(this).find('select').each(function(){
         if($(this).val().length == 0){
-         $("<span>Field should not be empty!</span>").insertAfter( $(this));
-            $(this).addClass("has-error");
+         $("<span>Field should not be empty!</span>").insertAfter($(this));
+            $(this).closest(".form-group.row").addClass("has-error");
+            //$(this).addClass("has-error");
             //$('select').closest("div.form-group.row").addClass("has-error");
       }
     });
