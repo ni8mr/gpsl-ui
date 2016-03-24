e141921c8b6308d1016877cea8cfeba748d5c741
ni8mr2 <nilpaddo@gmail.com>
2016-03-24 16:27:34 +0600

    Add codes for validation. (v4 - Made the empty boxes red. )

--- submit.js
+++ submit.js
@@ -4,11 +4,13 @@
     $(this).find('input').each(function(){
       if($(this).val().length == 0){
          $("<span>Field should not be empty!</span>").insertAfter( $(this));
+          $('input').addClass("has-error");
       }
     });
       $(this).find('select').each(function(){
         if($(this).val().length == 0){
          $("<span>Field should not be empty!</span>").insertAfter( $(this));
+            $('select').addClass("has-error");
       }
     });
   });
