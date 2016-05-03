5e58ca3ec9b5410008bd62e9a2f1abd9aaac6cdd
ni8mr2 <nilpaddo@gmail.com>
2016-05-03 17:53:14 +0600

    Will add a "Add new b/l" button. - V11
       It will try to add 10 rows at the "B/L list" entry table. V16
     (It will align the rows. V35)

--- bl_list.js
+++ bl_list.js
@@ -11,13 +11,13 @@ $(document).ready(function () {
         e.preventDefault();
 
         // Removing previously generated dynamic fields
-        $("div#bl-list-id").empty().append("<br>");
+        $("div#bl-list-id").empty();
         $("div#bl-volume-number").empty().append("<label>Number</label>");
         $("div#bl-volume-type").empty().append("<label>Type</label>");
-        $("div#bl-status").empty().append("<br>");
+        $("div#bl-status").empty();
         $("div#bl-payable-at").empty().append("<br>");
-        $("div#bl-line-detention-checkbox").empty().append("<br>");
-        $("div#bl-line-detention-day-count").empty().append("<br>");
+        $("div#bl-line-detention-checkbox").empty();
+        $("div#bl-line-detention-day-count").empty();
 
 
         // var principal_name_url = location.origin + "/api/v1/principal/";
@@ -39,25 +39,25 @@ $(document).ready(function () {
         //Loading 10 dynamic rows initially for b/l list entry form
         for (i = 0; i < 10; i++) {
             // ID entry field generation
-            $("div#bl-list-id").append("<input type='text' name='bl-list-id'>" + "<br>");
+            $("div#bl-list-id").append("<br>" + "<input type='text' name='bl-list-id'>" + "<br>");
 
             // Volume-number entry field generation
-            $("div#bl-volume-number").append("<input type='text' name='bl-volume-number'>" + "<br>");
+            $("div#bl-volume-number").append("<br>" + "<input type='text' name='bl-volume-number'>" + "<br>");
 
             // Volume-type entry field generation
-            $("div#bl-volume-type").append("<input type='text' name='bl-volume-type'>" + "<br>");
+            $("div#bl-volume-type").append("<br>" + "<input type='text' name='bl-volume-type'>" + "<br>");
 
             // Status entry field generation
-            $("div#bl-status").append("<input type='text' name='bl-status'>" + "<br>");
+            $("div#bl-status").append("<br>" + "<input type='text' name='bl-status'>" + "<br>");
 
             // 'Payable at' selection field generation
             $("div#bl-payable-at").append("<select name='bl-payable-at'><option value='POD'>POD</option><option value='POL'>POL</option></select>");
 
             // Line detention checkbox generation
-            $("div#bl-line-detention-checkbox").append("<input type='checkbox' name='bl-line-detention-checkbox' checked>" + "<br>");
+            $("div#bl-line-detention-checkbox").append("<br>" + "<input type='checkbox' name='bl-line-detention-checkbox' checked>" + "<br>");
 
             // Line detention day-count entry field generation
-            $("div#bl-line-detention-day-count").append("<input type='number' name='bl-line-detention-day-count' placeholder='Free for (in days)'>" + "<br>");
+            $("div#bl-line-detention-day-count").append("<br>" + "<input type='number' name='bl-line-detention-day-count' placeholder='Free for (in days)'>" + "<br>");
         }
 
         /* Generating unique-id for different input fields */
