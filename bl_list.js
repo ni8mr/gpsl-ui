9a1c251a0515d2f1b88619251377ab93999358eb
ni8mr2 <nilpaddo@gmail.com>
2016-05-03 17:32:07 +0600

    Will add a "Add new b/l" button. - V11
       It will try to add 10 rows at the "B/L list" entry table. V16
     (It will align the rows. V33)

--- bl_list.js
+++ bl_list.js
@@ -39,25 +39,25 @@ $(document).ready(function () {
         //Loading 10 dynamic rows initially for b/l list entry form
         for (i = 0; i < 10; i++) {
             // ID entry field generation
-            $("div#bl-list-id").append("<br>" + "<input type='text' name='bl-list-id'>" + "<br>");
+            $("div#bl-list-id").append("<input type='text' name='bl-list-id'>");
 
             // Volume-number entry field generation
-            $("div#bl-volume-number").append("<br>" + "<input type='text' name='bl-volume-number'>" + "<br>");
+            $("div#bl-volume-number").append("<input type='text' name='bl-volume-number'>");
 
             // Volume-type entry field generation
-            $("div#bl-volume-type").append("<br>" + "<input type='text' name='bl-volume-type'>" + "<br>");
+            $("div#bl-volume-type").append("<input type='text' name='bl-volume-type'>");
 
             // Status entry field generation
-            $("div#bl-status").append("<br>" + "<input type='text' name='bl-status'>" + "<br>");
+            $("div#bl-status").append("<input type='text' name='bl-status'>");
 
             // 'Payable at' selection field generation
-            $("div#bl-payable-at").append("<br>" + "<select name='bl-payable-at'><option value='POD'>POD</option><option value='POL'>POL</option></select>" + "<br>");
+            $("div#bl-payable-at").append("<select name='bl-payable-at'><option value='POD'>POD</option><option value='POL'>POL</option></select>");
 
             // Line detention checkbox generation
-            $("div#bl-line-detention-checkbox").append("<br>" + "<input type='checkbox' name='bl-line-detention-checkbox' checked>" + "<br>");
+            $("div#bl-line-detention-checkbox").append("<input type='checkbox' name='bl-line-detention-checkbox' checked>");
 
             // Line detention day-count entry field generation
-            $("div#bl-line-detention-day-count").append("<br>" + "<input type='number' name='bl-line-detention-day-count' placeholder='Free for (in days)'>" + "<br>");
+            $("div#bl-line-detention-day-count").append("<input type='number' name='bl-line-detention-day-count' placeholder='Free for (in days)'>");
         }
 
         /* Generating unique-id for different input fields */
