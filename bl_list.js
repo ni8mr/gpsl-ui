0c6137374c49b41c765655f733a036daf77d4176
ni8mr2 <nilpaddo@gmail.com>
2016-05-04 10:06:02 +0600

    Try to align line detention checkbox field with other fields at the b/l entry form.

--- bl_list.js
+++ bl_list.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
             $("div#bl-status").append("<br>" + "<input type='text' name='bl-status'>" + "<br>");
 
             // 'Payable at' selection field generation
-            $("div#bl-payable-at").append("<br>" + "<select name='bl-payable-at'><option value='POD'>POD</option><option value='POL'>POL</option></select>" + "<br>");
+            $("div#bl-payable-at").append("<br>" + "<select name='bl-payable-at'><option value='POD'>POD</option><option value='POL'>POL</option></select>" + "<br>" + "<br>");
 
             // Line detention checkbox generation
             $("div#bl-line-detention-checkbox").append("<br>" + "<input type='checkbox' name='bl-line-detention-checkbox' checked>" + "<br>");
@@ -159,7 +159,7 @@ $(document).ready(function () {
         $("div#bl-payable-at").append("<br>" + "<select name='bl-payable-at' id='ui-id-" + payable_at + "'><option value='POD'>POD</option><option value='POL'>POL</option></select>" + "<br>");
 
         // Line detention checkbox generation
-        $("div#bl-line-detention-checkbox").append("<br>" + "<input type='checkbox' name='bl-line-detention-checkbox' id='ui-id-" + line_detention_checkbox + "' checked>" + "<br>");
+        $("div#bl-line-detention-checkbox").append("<br>" + "<input type='checkbox' name='bl-line-detention-checkbox' id='ui-id-" + line_detention_checkbox + "' checked>" + "<br>" + "<br>");
 
         // Line detention day-count entry field generation
         $("div#bl-line-detention-day-count").append("<br>" + "<input type='number' name='bl-line-detention-day-count' placeholder='Free for (in days)' id='ui-id-" + line_detention_day_count + "'>" + "<br>");
